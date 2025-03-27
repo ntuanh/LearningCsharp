50300814027236a080d4ab941b9cb8bdf9557343
ntuanh <ntuanh2705@gmail.com>
2025-03-27 16:49:52 +0000

    [UPLOAD] simplest syntax and display console

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class cars
+    {
+        public string color = "red";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{1} and {0}" ,byte.MinValue , byte.MaxValue);
+            cars mycar = new cars();
+            Console.WriteLine(mycar.color);
+        }
+    }   
+}
